66c9034dd340536f787f9610c65b1d3a4bb92626
MikhailPakaliuk <mr.dyurilen@mail.ru>
2019-10-17 13:21:47 +0300

    feat: add function zero_matrix

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  
+  function zero_matrix(value){
+    let result=0;
+    for(let i=0;i<value.length;i++){
+	    for(let j=0;j<value.length;j++){
+		    if (value[i][j]==0){
+		  	  result+=1;
+		    }
+	    }
+    }
+    return result;
+  }
+  
 }
